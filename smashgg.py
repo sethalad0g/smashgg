01adc354a4f99652bc065d3b6d379534402cc89c
sethalad0g <cephal0g0d@hotmail.com>
2020-06-09 10:46:03 -0400

    Added ratelimit package to smashgg.py to limit rate to 60 requests per minute. Added requirements.txt

--- smashgg.py
+++ smashgg.py
@@ -4,9 +4,14 @@ import requests
 import json
 import pandas as pd
 from datetime import datetime
-import time
+from ratelimit import limits, sleep_and_retry
 
-def gg_reqs(page, beforeDate, query, rnum):
+ONE_MINUTE=60
+
+#Decorators to limit API calls. SmashGG says limit is 80 per minute, average. I limited it to 60 per minute.
+@sleep_and_retry #If rate limit is reached, wait til end of period then try again
+@limits(calls=60, period=ONE_MINUTE) #60 calls per minute
+def gg_req(page, beforeDate, query):
 
     #Go to https://smashgg-developer-portal.netlify.app for documentation. They have a blog where\
     #you can post your projects
@@ -14,31 +19,26 @@ def gg_reqs(page, beforeDate, query, rnum):
     
     #Get token by making smashgg account
     token = #token goes here as string
-    
-    rlist = []
 
-    #Each run of this loop makes 1 request, need to set gql variables here so you can iterate
-    tracker = 0
-    while tracker < rnum:#rnum is number of request you want to make per run
-        variables = {
-          "page": page, #page returned by query
-          "beforeDate": beforeDate #Unix Timestamp
-        }
+    variables = {
+      "page": page, #Results divided into pages, this is which page you want
+      "beforeDate": beforeDate #Only returns data with startAt before this Unix Timestamp
+    }
 
-        myobject = {
-          "query": query,#graphql query as string
-            "variables": variables
-        }
+    myobject = {
+      "query": query, #Your graphql query as a string
+        "variables": variables
+    }
 
-        #Need your token to be in the Authorization header of your request
-        rlist.append(requests.post(url, json = myobject, headers = {"Authorization": "Bearer {}".format(token)}))
-        page += 1
-        tracker += 1
+    #You can use software to make your requests like Postman. Need your token to be in the
+    #Authorization header of your request
+    response = requests.post(url, json = myobject, headers = {"Authorization": "Bearer {}".format(token)})
 
-    #returns page for tracking progress, and rlist is a list of responses
-    return page, rlist
+    if response.status_code != 200:
+        raise Exception('API response: {}'.format(response.status_code))
+    return response
 
-#Each response comes back in json, this is to convert it into a pandas dataframe
+#Each response comes back in json, this is to a list of them into a pandas dataframe
 def tour_parser(rlist):
 
     #column names for the dataframe
@@ -59,9 +59,9 @@ def tour_parser(rlist):
      'venueName': []}
 
     #I just pulled each object directly; If query returns nothing objects come back as None
-    for req in rlist:
-        parsed = json.loads(req.text)
-        #On error, response will have 'error' instead of 'data
+    for resp in rlist:
+        parsed = json.loads(resp.text)
+        #On graphql error, response will have 'error' instead of 'data
         if 'data' in parsed and parsed['data'] is not None and parsed['data']['tournaments'] is not None\
         and parsed['data']['tournaments']['nodes'] is not None:
             for node in parsed['data']['tournaments']['nodes']:
@@ -145,34 +145,39 @@ df =  pd.DataFrame({'name': [],
      'rules': [],
      'venueName': []})
 
-#smashgg has a rate limit. 80 requests per minute average. 1000 objects per request. I had trouble finding a solution
-#to limit my requests to the rate automatically, so I tweaked these variables to avoid hitting it.
-#Still never found a solution. Seems like the api stops you when you get to 10000 objects in some unknown timeframe, 
+#smashgg has a rate limit. 80 requests per minute average. 1000 objects per request. I limited my api calls to 60/ minute but
+#still got stopped. Seems like the api stops you when you get to 10000 objects in some unknown timeframe, 
 #so future work is to convert this into a crawler that runs periodically once I get more info from devs
+                
 page = 1
 beforeDate = 1589932800
-#beforeDate = 1545435000
-rnum = 50
 
 loop = True
 while loop:
        
-    og_count = len(df.index)
-        
-    page, rlist = gg_reqs(page, beforeDate, query, rnum)
+    og_count = len(df.index) #Starting size of dataframe each loop
+    
+    rlist = []
+    badpages = []
+    
+    while page <= 200:
+        try:
+            rlist.append(gg_req(page,beforeDate,query))  
+        except Exception as e:
+            print("Exception: ",e,"Page: ",page)
+            badpages.append(page)
+            
+        page +=1
+    
     df = pd.concat([ df, tour_parser(rlist)])
     
-    new_count = len(df.index)
+    new_count = len(df.index) #Size of dataframe at end of loop
     
-    #Stop looping if more rows aren't added. At that point I would check if I hit the rate limit.
+    #Stop looping if more rows aren't added. Can add counter here to let it try more times before quitting.
     if og_count == new_count:
         loop = False
-        
-    time.sleep(120)
-
-     
-    #beforeDate = int(df['startAt'].sort_values().iloc[0])
-    
+       
+   
 #Add startDate and endDate columns for analysis
 #Then add data to csv
 startDate = df['startAt'].map(lambda val: datetime.utcfromtimestamp(int(val)).strftime('%m-%d-%Y')).rename("startDate")
